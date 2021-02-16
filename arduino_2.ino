9af71926303a39e38c18aa1021ba449801e05e70
donbasta <faddilafarras@gmail.com>
2021-02-16 16:17:37 +0700

    refactor arduino 2

--- arduino_2.ino
+++ arduino_2.ino
@@ -1,8 +1,6 @@
 #include <LiquidCrystal.h>
 
-//char bufferToA1[5] = "1";
-char bufferFromA1[6] = "0000";
-char bufferlul[6];
+char bufferFromA1[6];
 const char peopleExit[6] = "decrA";
 
 const int kPinPIR = 13;
@@ -12,14 +10,13 @@ int speedDC = 100;
 const int kPinDC = 10;
 
 int bufferFromA1ToInt() {
-  if (bufferlul[4] == 'A') {
-  	return speedDC;
-  }
+    if (bufferFromA1[4] == 'A') {
+        return speedDC;
+    }
 	int ret = 0;
   	int pw = 1;
   	for (int i = 0; i < 4; i++) {
-        int dig = (int) (bufferlul[i] - '0');
-        //Serial.println(dig);
+        int dig = (int) (bufferFromA1[i] - '0');
   		ret = ret + pw * dig;
     	pw = pw * 10;
   	}
@@ -35,23 +32,16 @@ void sendToA1() {
 }
 
 void recvFromA1() {
-	Serial.readBytes(bufferlul, 5);
+	Serial.readBytes(bufferFromA1, 5);
     speedDC = bufferFromA1ToInt();
-  	//Serial.print("current buffer: ");
-    //Serial.println(bufferlul);
-    //Serial.print("current retrieved speed: ");
-    //Serial.println(speedDC);
 }
 
 void loop() {
-  recvFromA1();
-  if (digitalRead(kPinPIR) == HIGH) {
-    //Serial.println("tesuto");
-    //Serial.println(speedDC);
-    analogWrite(kPinDC, speedDC);  
-    sendToA1();
-    delay(500);
-    analogWrite(kPinDC, LOW);
-  }
+    recvFromA1();
+    if (digitalRead(kPinPIR) == HIGH) {
+        analogWrite(kPinDC, speedDC);  
+        sendToA1();
+        delay(500);
+        analogWrite(kPinDC, LOW);
+    }
 }
-
